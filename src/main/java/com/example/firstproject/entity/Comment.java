153c6cda11067bd9af8e5fb44a00de1410770f06
Jang Jidam <jidam03@dongguk.edu>
2022-10-21 09:31:26 +0900

    lect 22ing

---
+++ src/main/java/com/example/firstproject/entity/Comment.java
@@ -0,0 +1,33 @@
+package com.example.firstproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne // 댓글 엔티티 여러개가 하나의 Article에 연관됨
+    @JoinColumn(name = "article_id") // article_id 컬럼에 연결된 Article의 대표값을 저장(필드명 지정)
+    private Article article;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String  body;
+
+
+
+
+}
